0b93db832c5d8027246a36ecdd864e1b4784d0e4
Martin Petr <martin.petr.276@gmail.com>
2024-05-22 21:34:31 +0200

    feat: setup wails ui

--- cmd/desktop/main.go
+++ cmd/desktop/main.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/options"
+	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
-	"mp.fencing.core/internal/core"
+	"mp.fencing.core/frontend"
 )
 
 func main() {
+	// Create an instance of the app structure
+	app := NewApp()
+
+	// Create application with options
+	err := wails.Run(&options.App{
+		Title:  "Fencing",
+		Width:  1024,
+		Height: 768,
+		AssetServer: &assetserver.Options{
+			Assets: frontend.Assets,
+		},
+		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		OnStartup:        app.startup,
+		Bind: []interface{}{
+			app,
+		},
+	})
+
+	if err != nil {
+		println("Error:", err.Error())
+	}
+
 	db, _ := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 
-	core.NewApp(db)
 }
