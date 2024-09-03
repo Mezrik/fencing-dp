df588602a871ccb1e76b1a3d77de2a0377dda39f
Martin Petr <martin.petr.276@gmail.com>
2024-09-03 18:06:12 +0200

    refactor: remove incorrectly named competitor app folder

--- internal/competitior/domain/repositories/club_repo.go
+++
@@ -1,8 +0,0 @@
-package repositories
-
-import "github.com/Mezrik/fencing-dp/internal/competitior/domain/entities"
-
-type ClubRepo interface {
-	Create(competitor *entities.Club) error
-	FindAll() ([]*entities.Club, error)
-}
