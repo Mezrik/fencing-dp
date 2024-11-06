a7ebb7f6c8872f51648673e403cf335efaf6ea7b
Martin Petr <martin.petr.276@gmail.com>
2024-11-06 21:09:24 +0100

    feat(frontend): enable language mutations switch

--- frontend/src/app/routes/app/settings.tsx
+++ frontend/src/app/routes/app/settings.tsx
@@ -1,9 +1,36 @@
 import { BasicPageLayout } from '@/components/layouts';
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from '@/components/ui/form';
+import { dynamicActivate, locales } from '@/i18n';
 import { t } from '@lingui/macro';
 import { QueryClient } from '@tanstack/react-query';
 
 export const settingsLoader = (queryClient: QueryClient) => async () => {};
 
 export const SettingsRoute = () => {
-  return <BasicPageLayout title={t`Settings`}>Something</BasicPageLayout>;
+  return (
+    <BasicPageLayout title={t`Settings`}>
+      <Select
+        onValueChange={(value) => {
+          dynamicActivate(value as keyof typeof locales);
+        }}
+      >
+        <SelectTrigger>
+          <SelectValue placeholder={t`Select a language`} />
+        </SelectTrigger>
+        <SelectContent>
+          {Object.entries(locales).map(([key, value]) => (
+            <SelectItem value={key} key={key}>
+              {value}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </BasicPageLayout>
+  );
 };
--- frontend/src/i18n.ts
+++ frontend/src/i18n.ts
@@ -1,15 +1,18 @@
-import { i18n } from "@lingui/core";
+import { i18n } from '@lingui/core';
 
 export const locales = {
-  en: "English",
-  cs: "Česky",
+  en: 'English',
+  cs: 'Česky',
 } as const;
 
-export const defaultLocale = "en";
+export const defaultLocale = 'en';
 
 export async function dynamicActivate(locale: keyof typeof locales) {
+  console.log(locale);
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
   const { messages } = await import(`./locales/${locale}.po`);
+
+  console.log(messages);
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
   i18n.load(locale, messages);
